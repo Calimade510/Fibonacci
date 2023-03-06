aef89b44194b013fc2e2b31027c47c8c8ccb3053
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-03-05 23:03:59 -0800

    Week 8 Lab 2

--- Week_8_lab_2.py
+++ Week_8_lab_2.py
@@ -7,8 +7,8 @@ def fib(n):
         return fib(n - 1) + fib(n - 2)
 def main():
     for i in range(16):
-        print(fib(i), end='. ')
-        print('...')
+        print(fib(i), end='.')
+    print('...')
 
 
 if __name__=='__main__':
